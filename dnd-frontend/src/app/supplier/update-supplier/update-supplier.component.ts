68048b2f8399b2d1953682e578437e56710465c2
Jaddo <kabhikabhimeredilmai@gmail.com>
2023-08-10 16:47:20 +0530

    done

--- dnd-frontend/src/app/supplier/update-supplier/update-supplier.component.ts
+++ dnd-frontend/src/app/supplier/update-supplier/update-supplier.component.ts
@@ -1,11 +1,6 @@
-<<<<<<< HEAD
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 
-=======
-import { Component } from '@angular/core';
-import { Router } from '@angular/router';
->>>>>>> 5b2cb0ac59714f625bc2d8c042a948ab18278b92
 import { SupplierService } from 'src/app/services/supplier.service';
 import { Supplier } from 'src/app/models/supplier.model';
 import { Observable } from 'rxjs';
@@ -39,7 +34,6 @@ export class UpdateSupplierComponent {
     // );
   }
 
-<<<<<<< HEAD
   updateSupplier() {
     this.service.updateSupplier(this.supplier);
     // .subscribe(
@@ -50,17 +44,6 @@ export class UpdateSupplierComponent {
     //   },
     //   (error:any) => console.log(error)
     // );
-=======
-  updateSupplier():Observable<any> {
-    this.service.updateSupplier(this.supplier).subscribe(
-      (data:Supplier) => {
-        console.log(data);
-        this.supplier = new Supplier();
-        this.gotoList();
-      },
-      (error:any) => console.log(error)
-    );
->>>>>>> 5b2cb0ac59714f625bc2d8c042a948ab18278b92
   }
   onSubmit() {
     this.updateSupplier();
